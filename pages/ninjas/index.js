68e30d1a80015885df580911833b74121d546602
Cormac Pujals <cmpujals@gmail.com>
2023-02-01 13:45:12 -0800

    more dynamic mapping

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users")
   const data = await res.json();
@@ -12,9 +13,9 @@ const Ninjas = ({ninjas}) => {
     <div>
       <h1>All Ninjas</h1>
       {ninjas.map(ninja => (
-        <div key={ninja.id}>
-          <a><h3>{ninja.name}</h3></a>
-        </div>
+        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+          <h3>{ninja.name}</h3>
+        </Link>
       ))}
     </div>
    );
